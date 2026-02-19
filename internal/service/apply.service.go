d1ab9d9468fdb173606a777f5ea7375b12afa23a
asnaweevixvify <asnaweeezor@gmail.com>
2026-02-19 10:35:28 +0700

    add field in apply service

--- internal/service/apply.service.go
+++ internal/service/apply.service.go
@@ -24,6 +24,13 @@ func (s *ApplyService) ApplyCamp(req dto.ApplyRequest, userID uuid.UUID) (dto.Ap
 			ID:        uuid.New(),
 			Teamname:  req.Teamname,
 			School:    req.School,
+			Call_1:    req.Call_1,
+			Call_2:    req.Call_2,
+			Relation:  req.Relation,
+			Name_1:    req.Name_1,
+			Name_2:    req.Name_2,
+			Name_3:    req.Name_3,
+			Name_4:    req.Name_4,
 			UserID:    userID,
 			CreatedAt: time.Now(),
 		}
@@ -35,6 +42,12 @@ func (s *ApplyService) ApplyCamp(req dto.ApplyRequest, userID uuid.UUID) (dto.Ap
 		q := models.Quiz{
 			ID:        uuid.New(),
 			Verified:  req.Verified,
+			Video:     req.Video,
+			Quiz_1:    req.Quiz_1,
+			Quiz_2:    req.Quiz_2,
+			Quiz_3:    req.Quiz_3,
+			Quiz_4:    req.Quiz_4,
+			Quiz_5:    req.Quiz_5,
 			UserID:    userID,
 			CreatedAt: time.Now(),
 		}
