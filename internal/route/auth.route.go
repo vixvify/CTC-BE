9acebecb4f94d9e5c64155d7b9a2bb40af3e3ded
asnaweevixvify <asnaweeezor@gmail.com>
2026-02-20 11:50:27 +0700

    ➕ add update, reset password, and delete routes to auth handler

--- internal/route/auth.route.go
+++ internal/route/auth.route.go
@@ -11,6 +11,9 @@ func RegisterAuthRoutes(r *gin.RouterGroup, h *handler.AuthHandler) {
 	r.Use(middleware.RateLimitMiddleware())
 	r.POST("/signup", h.Signup)
 	r.POST("/login", h.Login)
+	r.PUT("/update", h.Update)
+	r.PUT("/reset-password", h.ResetPassword)
+	r.DELETE("/delete", h.Delete)
 	r.GET("/me", h.Me)
 	r.POST("/logout", h.Logout)
 }
